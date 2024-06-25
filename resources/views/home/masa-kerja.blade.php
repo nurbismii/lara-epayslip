8927d7904d21c753cf9b8dbd67e7cf4d386b22f6
nurbismii <nurbismi74@gmail.com>
2024-06-25 16:32:48 +0800

    test: color chart

--- resources/views/home/masa-kerja.blade.php
+++ resources/views/home/masa-kerja.blade.php
@@ -448,9 +448,9 @@
 			label: 'Total',
 			data: grand_total_mk,
 			backgroundColor: [
-				'rgba(153, 102, 255, 0.8)',
-				'rgba(255, 99, 132, 0.8)',
+				'rgba(75, 192, 192, 0.8)',
 				'rgba(255, 159, 64, 0.8)',
+				'rgba(255, 99, 132, 0.8)',
 			],
 		}]
 	};
