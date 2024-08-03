e6f13b2b981eb144f8bdf41e6da8f88b79a0d816
Nurbismii <nurbismi74@gmail.com>
2024-08-03 09:10:07 +0800

    test: update route

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::group(['middleware' => 'is_active'], function () {
   });
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('is_active');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('is_active');
 Route::get('/masa-kerja', [App\Http\Controllers\HomeController::class, 'masaKerja'])->name('masaKerja')->middleware('is_admin');
 Route::get('/bpjs-tk', [App\Http\Controllers\HomeController::class, 'BPJSTK'])->name('bpjs-tk')->middleware('is_admin');
 Route::get('api/masa-kerja', [App\Http\Controllers\HomeController::class, 'fetchMasaKerja'])->middleware('is_admin');
