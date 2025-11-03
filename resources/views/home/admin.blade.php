17d92126d0c47bed7465b7fabe6578d9457439ed
Nurbismii <nurbismi74@gmail.com>
2025-11-03 10:42:55 +0800

    test: fix typo list tahun

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -111,8 +111,8 @@
                         <form action="" method="get">
                             <select name="tahun" class="form-control mb-2">
                                 <option value="2027">2027</option>
-                                <option value="2026">2024</option>
-                                <option value="2025">2024</option>
+                                <option value="2026">2026</option>
+                                <option value="2025">2025</option>
                                 <option value="2024">2024</option>
                                 <option value="2023">2023</option>
                             </select>
